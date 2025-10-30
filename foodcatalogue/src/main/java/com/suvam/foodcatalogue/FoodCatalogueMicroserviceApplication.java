9b23a7995dffce0d48bef06304dfe3b6037e4068
Suvam Debnath <debnathsuvam322@gmail.com>
2025-10-30 23:11:19 +0530

    added all layers in foodcatalogue microservice

--- foodcatalogue/src/main/java/com/suvam/foodcatalogue/FoodCatalogueMicroserviceApplication.java
+++ foodcatalogue/src/main/java/com/suvam/foodcatalogue/FoodCatalogueMicroserviceApplication.java
@@ -2,6 +2,9 @@ package com.suvam.foodcatalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FoodCatalogueMicroserviceApplication {
@@ -9,5 +12,10 @@ public class FoodCatalogueMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FoodCatalogueMicroserviceApplication.class, args);
 	}
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
