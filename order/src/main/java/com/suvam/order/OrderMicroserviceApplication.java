fe224f50a89e837787d0cbe729be079635bf2a06
Suvam Debnath <debnathsuvam322@gmail.com>
2025-11-02 12:51:15 +0530

    added all layers in order MS

--- order/src/main/java/com/suvam/order/OrderMicroserviceApplication.java
+++ order/src/main/java/com/suvam/order/OrderMicroserviceApplication.java
@@ -2,6 +2,9 @@ package com.suvam.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrderMicroserviceApplication {
@@ -9,5 +12,10 @@ public class OrderMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMicroserviceApplication.class, args);
 	}
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
